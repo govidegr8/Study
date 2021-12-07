6f5da86aaafc20573d6b9dc99f4e996698b670eb
govidegr8 <66997969+govidegr8@users.noreply.github.com>
2021-12-07 20:31:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+//import 'antd/dist/antd.css';
+
+//import AppHeader from './components/common/header';
+//import AppFooter from './components/common/footer';
+//import AppHome from './views/home';
+
+//import { Layout } from 'antd';
+import Index from './users/Index';
+//const { Header, Content, Footer } = Layout;
+
+function App() {
+  return (
+    
+      <Index/>    
+   
+  );
+}
+
+export default App;
---
+++ UserData.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const user_data = "user data";
+function UserData(){
+    return <h1>{user_data}</h1>
+}
+export default UserData;
